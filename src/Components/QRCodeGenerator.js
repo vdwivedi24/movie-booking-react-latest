dd754c394e321c37f6622b99b2ba49319eccaee3
vikrant dwivedi <vkrntdwivedi224@gmail.com>
2021-07-02 14:45:47 -0400

    Formatted file using prettier

--- src/Components/QRCodeGenerator.js
+++ src/Components/QRCodeGenerator.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import { QRCode } from 'react-qrcode-logo';
-
+import React from "react";
+import { QRCode } from "react-qrcode-logo";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function QRCodeGenerator(props) {
-const dataForQRCode = `${props.name} ${props.image} ${props.actors}`
+  const dataForQRCode = `${props.name} ${props.image} ${props.actors}`;
   return (
     <div>
-    <QRCode value={dataForQRCode} /> 
-    <p>Congratulations! You have suvvessfully booked the ticket for <strong>{props.name}</strong>. Enjoy your time.</p>
+      <QRCode value={dataForQRCode} />
+      <p>
+        <strong>Congratulations!</strong> You have successfully booked the
+        ticket for <strong>{props.name}</strong>. Enjoy your time.
+      </p>
+      <p>
+        <strong>Number of seats:</strong> {props.seats}
+      </p>
+      <p>
+        <strong>Plot:</strong> {props.plot}
+      </p>
+      <p>
+        <strong>Genres:</strong> {props.genres}
+      </p>
+      <p>
+        <strong>Actors:</strong> {props.actors}
+      </p>
+      <Button
+        style={{
+          width: "25rem",
+          marginTop: "1rem",
+          marginBottom: "1rem",
+          marginLeft: "1rem",
+          paddingRight: "1rem",
+          marginBottom: "1rem",
+        }}
+        variant="dark"
+        size="md"
+      >
+        <Link to={`/`}>Back to Movie List</Link>
+      </Button>
     </div>
-     );
+  );
 }
 export default QRCodeGenerator;
