9d5beb6aebd56ec0600b20d755c2c53b52884513
vikrant dwivedi <vkrntdwivedi224@gmail.com>
2021-07-02 14:46:17 -0400

    Formatted file using prettier

--- src/Components/UpcomingMovies.js
+++ src/Components/UpcomingMovies.js
@@ -1,29 +1,38 @@
 import ImageCarousel from "./ImageCarousel";
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import MovieList from "./MovieList";
-
+import MovieList from "./movies/MovieList";
 
 class UpcomingMovies extends Component {
   componentDidMount() {
-    this.props.onRequestUpcomingMovies()
+    this.props.onRequestUpcomingMovies();
   }
   render() {
-    return this.props.upcomingmovies && 
-    <div>
-    <MovieList movieList={this.props.upcomingmovies} isUpcoming={true} />
-    </div>
+    return (
+      this.props.upcomingmovies && (
+        <div
+          style={{
+            padding: "5rem",
+            paddingTop: "1rem",
+          }}
+        >
+          <h3>Upcoming movies</h3>
+          <MovieList movieList={this.props.upcomingmovies} isUpcoming={true} />
+        </div>
+      )
+    );
   }
 }
 const mapStateToProps = (state) => {
   return {
-    upcomingmovies: state.sagaUpcomingMoviesReducer }
+    upcomingmovies: state.sagaUpcomingMoviesReducer,
+  };
 };
-
 
 function mapDispatchToProps(dispatch) {
   return {
-    onRequestUpcomingMovies : () => dispatch({ type: 'UPCOMINGMOVIES_FETCH_REQUESTED' }),
+    onRequestUpcomingMovies: () =>
+      dispatch({ type: "UPCOMINGMOVIES_FETCH_REQUESTED" }),
   };
 }
 
