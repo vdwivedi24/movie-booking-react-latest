e1c175563a1f8ce776c2ff704ea10fda9f93674c
vikrant dwivedi <vkrntdwivedi224@gmail.com>
2021-07-02 14:44:50 -0400

    Formatted file using prettier

--- src/Components/ImageCarousel.js
+++ src/Components/ImageCarousel.js
@@ -1,13 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Carousel } from "react-bootstrap";
 
-
 function ImageCarousel() {
-    const [products, setProducts] = useState([]);
-    const [index, setIndex] = useState(0);
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
-      };
+  const [products, setProducts] = useState([]);
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
   useEffect(() => {
     fetch("http://localhost:3002/carousel", {
       method: "GET",
@@ -19,24 +18,30 @@ function ImageCarousel() {
   }, []);
 
   return (
-    <Carousel
-      activeIndex={index} onSelect={handleSelect}
-    >
+    <Carousel activeIndex={index} onSelect={handleSelect}>
       {products?.map((item) => (
-        <Carousel.Item interval={5000}
-        key={item.info.rank}
-        style={{ backgroundColor: '#FF9933' }}
-         >
+        <Carousel.Item
+          interval={5000}
+          key={item.info.rank}
+          style={{ backgroundColor: "#FF9933" }}
+        >
           <img
             className="photo"
             src={item.info.image_url}
             alt={item.title}
-            style={{ width: '30rem',  marginRight:'.5rem', padding:'.5rem', paddingTop:'1rem', paddingBottom:'5rem'}}
+            style={{
+              width: "30rem",
+              marginRight: ".5rem",
+              padding: ".5rem",
+              paddingTop: "1rem",
+              paddingBottom: "5rem",
+            }}
           />
           <Carousel.Caption
-          style={{ paddingBottom:'.2rem',paddingTop:'.2rem'}}>
-           <p>{item.info.plot}</p>
-        </Carousel.Caption>
+            style={{ paddingBottom: ".2rem", paddingTop: ".2rem" }}
+          >
+            <p>{item.info.plot}</p>
+          </Carousel.Caption>
         </Carousel.Item>
       ))}
     </Carousel>
