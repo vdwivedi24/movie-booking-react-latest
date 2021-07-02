dbd25c2aeb749fa1f2b7488bce8989a74de7610a
vikrant dwivedi <vkrntdwivedi224@gmail.com>
2021-07-02 14:42:34 -0400

    Formatted file using Prettier

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { render } from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import App from './App';
-import rootReducer from './reducers'
-import createSagaMiddleware from 'redux-saga'
-import mySaga from './sagas'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
+import rootReducer from "./reducers";
+import createSagaMiddleware from "redux-saga";
+import mySaga from "./sagas";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const sagaMiddleware = createSagaMiddleware()
-const store = createStore(
-  rootReducer,
-  applyMiddleware(sagaMiddleware));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
-
-sagaMiddleware.run(mySaga)
+sagaMiddleware.run(mySaga);
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-document.getElementById('root'));
-
+  document.getElementById("root")
+);
