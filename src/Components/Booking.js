5f3b2a870819f6f87d84cbbb4cb80206ede761b3
vikrant dwivedi <vkrntdwivedi224@gmail.com>
2021-07-02 15:36:18 -0400

    Refactor Booking.js and QRCodeGenerator.js

--- src/Components/Booking.js
+++ src/Components/Booking.js
@@ -1,6 +1,8 @@
 import React, { Component, useEffect } from "react";
 import QRCodeGenerator from "./QRCodeGenerator";
 import { connect } from "react-redux";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Booking(props) {
   useEffect(() => {
@@ -13,14 +15,47 @@ function Booking(props) {
       }}
     >
       {props.bookedMovie && (
-        <QRCodeGenerator
-          name={props.bookedMovie.name}
-          image={props.bookedMovie.image}
-          actors={props.bookedMovie.actors}
-          seats={props.bookedMovie.seats}
-          plot={props.bookedMovie.plot}
-          genres={props.bookedMovie.genres}
-        />
+        <div>
+          <QRCodeGenerator
+            name={props.bookedMovie.name}
+            image={props.bookedMovie.image}
+            actors={props.bookedMovie.actors}
+            seats={props.bookedMovie.seats}
+            plot={props.bookedMovie.plot}
+            genres={props.bookedMovie.genres}
+          />
+          <p>
+            <strong>Congratulations!</strong> You have successfully booked the
+            ticket for <strong>{props.bookedMovie.name}</strong>. Enjoy your
+            time.
+          </p>
+          <p>
+            <strong>Number of seats:</strong> {props.bookedMovie.seats}
+          </p>
+          <p>
+            <strong>Plot:</strong> {props.bookedMovie.plot}
+          </p>
+          <p>
+            <strong>Genres:</strong> {props.bookedMovie.genres}
+          </p>
+          <p>
+            <strong>Actors:</strong> {props.bookedMovie.actors}
+          </p>
+          <Button
+            style={{
+              width: "25rem",
+              marginTop: "1rem",
+              marginBottom: "1rem",
+              marginLeft: "1rem",
+              paddingRight: "1rem",
+              marginBottom: "1rem",
+            }}
+            variant="dark"
+            size="md"
+          >
+            <Link to={`/`}>Back to Movie List</Link>
+          </Button>
+        </div>
       )}
     </div>
   );
