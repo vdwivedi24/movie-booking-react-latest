17ebeab2c9768375b99bec8e89235bfab895c3ad
vikrant dwivedi <vkrntdwivedi224@gmail.com>
2021-07-02 14:44:01 -0400

    Style change for Events page

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,30 +1,37 @@
 import ImageCarousel from "./ImageCarousel";
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import MovieList from "./MovieList";
-
+import MovieList from './movies/MovieList';
 
 class Events extends Component {
   componentDidMount() {
-    this.props.events()
+    this.props.events();
   }
   render() {
-      return this.props.eventData && 
-      <div>
-      <MovieList movieList={this.props.eventData} isUpcoming={true} />
-      </div>
-}
+    return (
+      this.props.eventData && (
+        <div
+          style={{
+            padding: "5rem",
+            paddingTop: "1rem",
+          }}
+        >
+          <h3>Events</h3>
+          <MovieList movieList={this.props.eventData} isUpcoming={true} />
+        </div>
+      )
+    );
+  }
 }
 const mapStateToProps = (state) => {
   return {
-    eventData: state.sagaEvents }
+    eventData: state.sagaEvents,
+  };
 };
-
 
 function mapDispatchToProps(dispatch) {
   return {
-    events : () => dispatch({ type: 'FETCH_EVENTS' }),
-
+    events: () => dispatch({ type: "FETCH_EVENTS" }),
   };
 }
 
