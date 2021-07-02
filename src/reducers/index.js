25f28d243ece8ac0890585eb956f89afdb786d99
vikrant dwivedi <vkrntdwivedi224@gmail.com>
2021-07-02 14:41:17 -0400

    Added reducer function for Events and Upcoming movies

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,44 +1,50 @@
-import { combineReducers } from 'redux'
-
-
+import { combineReducers } from "redux";
 
 const sagaMoviesReducer = (state = [], action) => {
   switch (action.type) {
-    case 'MOVIES_FETCH_SUCCEEDED':
-      return action.movies
-    case 'MOVIES_FETCH_FAILED':
-      return action.message 
+    case "MOVIES_FETCH_SUCCEEDED":
+      return action.movies;
+    case "MOVIES_FETCH_FAILED":
+      return action.message;
     default:
-      return state
+      return state;
   }
-}
+};
 
 const sagaUpcomingMoviesReducer = (state = [], action) => {
-    switch (action.type) {
-      case 'UPCOMINGMOVIES_FETCH_SUCCEEDED':
-        return action.movies
-      case 'UPCOMINGMOVIES_FETCH_FAILED':
-        return action.message
-      default:
-        return state
-    }
+  switch (action.type) {
+    case "UPCOMINGMOVIES_FETCH_SUCCEEDED":
+      return action.movies;
+    case "UPCOMINGMOVIES_FETCH_FAILED":
+      return action.message;
+    default:
+      return state;
   }
+};
 
 const sagaEvents = (state = [], action) => {
   switch (action.type) {
-    case 'EVENTS_FETCH_SUCCEEDED':
-      return action.events
-    case 'EVENTS_FETCH_FAILED':
-      return action.message
+    case "EVENTS_FETCH_SUCCEEDED":
+      return action.events;
+    case "EVENTS_FETCH_FAILED":
+      return action.message;
     default:
-      return state
+      return state;
   }
-}
+};
 
-
+const bookedmoviereducer = (state = [], action) => {
+  switch (action.type) {
+    case "BOOKED_MOVIE":
+      return action.data;
+    default:
+      return state;
+  }
+};
 
 export default combineReducers({
   sagaUpcomingMoviesReducer,
   sagaEvents,
   sagaMoviesReducer,
-})
+  bookedmoviereducer,
+});
